e2e2b1491fd411feca72d5ff2befeed31d58e67f
NickW16 <171855220+NickW16@users.noreply.github.com>
2024-09-25 19:34:41 -0300

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(num, ...excl) {
+    const newArray = [];
+    num.forEach((item) => {
+        if (!excl.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
+
+removeFromArray([1,2,3,4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
