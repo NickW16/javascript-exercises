278e49360607dfdb652d91d4e5c6340c05a88bd9
NickW16 <171855220+NickW16@users.noreply.github.com>
2024-09-26 10:00:11 -0300

    Finish Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(par1, par2) {
+    if (!Number.isInteger(par1) || !Number.isInteger(par2)) return "ERROR";
+    if (par1 < 0 || par2 < 0) return "ERROR";
+    if (par1 > par2) {
+      const temp = par1;
+      par1 = par2;
+      par2 = temp;
+    }
+    let sum = 0;
+ 
+    for (i = par1; i <= par2; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
